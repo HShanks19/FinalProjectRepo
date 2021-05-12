5c66d763345dc8a8056eb558a1b3daab4e01b65d
MeganMette <Meganmette.1@gmail.com>
2021-05-12 15:50:32 +0100

    seperating out citizen profile and biography. Started routing citizen profile into app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Route, Router } from 'react-router-dom';
 import SearchForm from './SearchForm/SearchForm';
 import Footer from './Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,6 +6,7 @@ import './App.css';
 import './SearchForm/SearchForm.css';
 import './Footer/Footer.css';
 import redsearchText from './redsearchText.svg';
+import CitizenPage from './CitizenProfilePage/CitizenProfile';
 
 function App() {
   return (
@@ -12,6 +14,11 @@ function App() {
       <div className="App">
         <img src={redsearchText} alt="Logo" style={{ height: 120, width: 900, margin: 50 }} />
         <SearchForm />
+        <Router>
+          <Route>
+            <CitizenPage />
+          </Route>
+        </Router>
         <Footer />
       </div>
     </>
--- frontend/src/CitizenProfilePage/CitizenProfile.jsx
+++ frontend/src/CitizenProfilePage/CitizenProfile.jsx
@@ -1,13 +1,10 @@
 import {
   Button, Row, Col, Container,
 } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import Biography from './Biography/Biography';
 import './CitizenProfile.css';
 
-const CitizenPage = ({
-  // eslint-disable-next-line max-len
-  firstName, lastName, dateOfBirth, placeOfBirth, address, phoneNumber, vehicleRegistrationPlate, vehicleMake, vehicleModel, vehicleColour,
-}) => (
+const CitizenPage = () => (
   <>
     <Row>
       <Col>
@@ -22,75 +19,11 @@ const CitizenPage = ({
     <Container>
       <Row>
         <Col>
-          <div className="card">
-            <div className="card-body">
-              <h4 className="card-title">
-                Citizen Name:
-                {' '}
-                {firstName + lastName}
-              </h4>
-              <h5 className="card-text">
-                Date of Birth:
-                {' '}
-                {dateOfBirth}
-              </h5>
-              <h5 className="card-text">
-                Place of Birth:
-                {' '}
-                {placeOfBirth}
-              </h5>
-              <h5 className="card-text">
-                Address:
-                {' '}
-                {address}
-              </h5>
-              <h5 className="card-text">
-                Phone Number:
-                {' '}
-                {phoneNumber}
-              </h5>
-              <h5 className="card-text">
-                Vehicle:
-              </h5>
-              <h5 className="card-text">
-                Registration Plate:
-                {' '}
-                {vehicleRegistrationPlate}
-              </h5>
-              <h5 className="card-text">
-                Make:
-                {' '}
-                {vehicleMake}
-              </h5>
-              <h5 className="card-text">
-                Model:
-                {' '}
-                {vehicleModel}
-              </h5>
-              <h5 className="card-text">
-                Colour:
-                {' '}
-                {vehicleColour}
-              </h5>
-            </div>
-          </div>
+          <Biography />
         </Col>
       </Row>
     </Container>
   </>
 );
-
-CitizenPage.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  dateOfBirth: PropTypes.string.isRequired,
-  placeOfBirth: PropTypes.string.isRequired,
-  address: PropTypes.string.isRequired,
-  phoneNumber: PropTypes.string.isRequired,
-  vehicleRegistrationPlate: PropTypes.string.isRequired,
-  vehicleMake: PropTypes.string.isRequired,
-  vehicleModel: PropTypes.string.isRequired,
-  vehicleColour: PropTypes.string.isRequired,
-};
 
 export default CitizenPage;
