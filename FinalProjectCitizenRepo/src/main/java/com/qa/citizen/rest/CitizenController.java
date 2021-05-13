9e810c5e1bbc4a1a8c545da9f5ec39b3fea65916
jpqa1 <jplatt@qa.com>
2021-05-13 09:32:30 +0100

    Functionality to retrieve citizen from user search added

--- FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/CitizenController.java
+++ FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/CitizenController.java
@@ -1,8 +1,11 @@
 package com.qa.citizen.rest;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.citizen.domain.Citizen;
@@ -18,10 +21,25 @@ public class CitizenController {
 		super();
 		this.service = service;
 	}
-	
+
 	@GetMapping("/getCitizen/{id}")
 	public Citizen getCitizenById(@PathVariable Long id) {
 		return this.service.getCitizenById(id);
+	}
+
+//	@GetMapping("/getCitizenFromSearch")
+//	public List<Citizen> getCitizenFromSearchForm(@RequestBody Citizen citizen) {
+//		Citizen foundCitizen = citizen;
+//
+//		return this.service.findCitizenByCompleteSearch(foundCitizen.getForenames(), foundCitizen.getSurname(),
+//				foundCitizen.getHomeAddress(), foundCitizen.getDateOfBirth(), foundCitizen.getPlaceOfBirth());
+//	}
+
+	@GetMapping("/getCitizenFromSearch")
+	public List<Citizen> getCitizenFromSearchForm(@RequestBody Citizen citizen) {
+		Citizen foundCitizen = citizen;
+
+		return this.service.findCitizenByCompleteSearch(foundCitizen);
 	}
 
 }
