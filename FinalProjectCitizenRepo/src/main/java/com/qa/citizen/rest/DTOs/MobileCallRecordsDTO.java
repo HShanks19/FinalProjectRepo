b11df8797475bc7299a052ea81d21d5fd790040f
holly-shanks <HShanks@qa.com>
2021-05-14 14:31:10 +0100

    Working Search for Call History

---
+++ FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/DTOs/MobileCallRecordsDTO.java
@@ -0,0 +1,57 @@
+package com.qa.citizen.rest.DTOs;
+
+public class MobileCallRecordsDTO {
+
+	public MobileCallRecordsDTO(String timestamp, String callerMSISDN, Long callCellTowerId, String receiverMSISDN) {
+		super();
+		this.timestamp = timestamp;
+		this.callerMSISDN = callerMSISDN;
+		this.callCellTowerId = callCellTowerId;
+		this.receiverMSISDN = receiverMSISDN;
+	}
+
+	public MobileCallRecordsDTO() {
+		super();
+	}
+	
+	private String timestamp;
+		
+	private String callerMSISDN;
+		
+	private Long callCellTowerId;
+	
+	private String receiverMSISDN;
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getCallerMSISDN() {
+		return callerMSISDN;
+	}
+
+	public void setCallerMSISDN(String callerMSISDN) {
+		this.callerMSISDN = callerMSISDN;
+	}
+
+	public Long getCallCellTowerId() {
+		return callCellTowerId;
+	}
+
+	public void setCallCellTowerId(Long callCellTowerId) {
+		this.callCellTowerId = callCellTowerId;
+	}
+
+	public String getReceiverMSISDN() {
+		return receiverMSISDN;
+	}
+
+	public void setReceiverMSISDN(String receiverMSISDN) {
+		this.receiverMSISDN = receiverMSISDN;
+	}
+
+}
---
+++ FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/DTOs/PeopleMobileDTO.java
@@ -0,0 +1,53 @@
+package com.qa.citizen.rest.DTOs;
+
+import java.util.Set;
+
+public class PeopleMobileDTO {
+
+	
+	public PeopleMobileDTO(String phoneNumber, String network, Set<MobileCallRecordsDTO> mobileCallRecords) {
+		super();
+		this.phoneNumber = phoneNumber;
+		this.network = network;
+		this.mobileCallRecords = mobileCallRecords;
+	}
+
+	public PeopleMobileDTO() {
+		super();
+	}
+	
+	private String phoneNumber;
+	
+	private String network;
+
+	private Set<MobileCallRecordsDTO> mobileCallRecords;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getNetwork() {
+		return network;
+	}
+
+	public void setNetwork(String network) {
+		this.network = network;
+	}
+
+	public Set<MobileCallRecordsDTO> getMobileCallRecords() {
+		return mobileCallRecords;
+	}
+
+	public void setMobileCallRecords(Set<MobileCallRecordsDTO> mobileCallRecords) {
+		this.mobileCallRecords = mobileCallRecords;
+	}
+
+	
+
+	
+
+}
