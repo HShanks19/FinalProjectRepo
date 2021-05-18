33e0156bf6a6448ac209e7a4ac8ca55131818d32
jpqa1 <jplatt@qa.com>
2021-05-18 12:09:53 +0100

    merge to bring branch up to date

--- FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/PeopleBankAccountController.java
+++ FinalProjectCitizenRepo/src/main/java/com/qa/citizen/rest/PeopleBankAccountController.java
@@ -1,8 +1,13 @@
 package com.qa.citizen.rest;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.citizen.domain.PeopleBankAccount;
@@ -19,10 +24,16 @@ public class PeopleBankAccountController {
 		super();
 		this.service = service;
 	}
-		
+
 	@GetMapping("/getBankAccountDTO/{accountNumber}")
 	public PeopleBankAccountDTO getBankAccountByIDDTO(@PathVariable Long accountNumber) {
 		return this.service.getDTO(accountNumber);
+	}
+
+	@PostMapping("/getMatchingBankAccounts/")
+	public ResponseEntity<List<PeopleBankAccount>> sortAndFilterBankAccounts(
+			@RequestBody PeopleBankAccount peopleBankAccount) {
+		return ResponseEntity.ok(this.service.sortAndFilterBankAccounts(peopleBankAccount));
 	}
 
 }
