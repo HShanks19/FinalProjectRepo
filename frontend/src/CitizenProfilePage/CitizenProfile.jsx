08a149a7a08d59011977a12a22348d01f40b8162
MeganMette <Meganmette.1@gmail.com>
2021-05-19 15:29:04 +0100

    nesting Biography, Associates, Finances and Whereabouts into CitizenProfile. Not rendering content to pages.

--- frontend/src/CitizenProfilePage/CitizenProfile.jsx
+++ frontend/src/CitizenProfilePage/CitizenProfile.jsx
@@ -1,5 +1,9 @@
+/* eslint no-template-curly-in-string: "error" */
 import {
-  Button, Row, Col, Container,
+  BrowserRouter as Route, Switch, Link, useRouteMatch,
+} from 'react-router-dom';
+import {
+  Button, Row, Col,
 } from 'react-bootstrap';
 import Biography from './Biography/Biography';
 import Finances from './FinanceHistory/Finances';
@@ -7,35 +11,36 @@ import Associates from './Associates/Associates';
 import './CitizenProfile.css';
 import Whereabouts from './Whereabouts/Whereabouts';
 
-const CitizenPage = () => (
-  <>
-    <Row>
-      <Col>
-        <div className="d-grid d-md-block">
-          <Button className="btn" type="button" id="button1" variant="outline-danger" size="lg">Biography</Button>
-          <Button className="btn" type="button" id="button2" variant="outline-danger" size="lg">Associates</Button>
-          <Button className="btn" type="button" id="button3" variant="outline-danger" size="lg">Financial History</Button>
-          <Button className="btn" type="button" id="button4" variant="outline-danger" size="lg">Whereabouts</Button>
-        </div>
-      </Col>
-    </Row>
-    <Container>
+const CitizenPage = () => {
+  const { path } = useRouteMatch();
+  return (
+    <>
       <Row>
         <Col>
-          <Biography />
-        </Col>
-        <Col>
-          <Associates />
-        </Col>
-        <Col>
-          <Finances />
-        </Col>
-        <Col>
-          <Whereabouts />
+          <div className="d-grid d-md-block">
+            <Button className="btn" type="button" id="button1" variant="outline-danger" size="lg"><Link to="/Biography">Biography</Link></Button>
+            <Button className="btn" type="button" id="button2" variant="outline-danger" size="lg"><Link to="/Associates">Associates</Link></Button>
+            <Button className="btn" type="button" id="button3" variant="outline-danger" size="lg"><Link to="/Finances">Financial History</Link></Button>
+            <Button className="btn" type="button" id="button4" variant="outline-danger" size="lg"><Link to="/Whereabouts">Whereabouts</Link></Button>
+          </div>
         </Col>
       </Row>
-    </Container>
-  </>
-);
+      <Switch>
+        <Route path={`${path}/Biography`}>
+          <Biography />
+        </Route>
+        <Route path={`${path}/Associates`}>
+          <Associates />
+        </Route>
+        <Route path={`${path}/Finances`}>
+          <Finances />
+        </Route>
+        <Route path={`${path}/Whereabouts`}>
+          <Whereabouts />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default CitizenPage;
