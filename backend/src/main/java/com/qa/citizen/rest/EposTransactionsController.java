d5903aa64bd54a0be8a910894afd816c9baf7a37
jpqa1 <jplatt@qa.com>
2021-05-20 13:16:47 +0100

    fixed error

--- backend/src/main/java/com/qa/citizen/rest/EposTransactionsController.java
+++ backend/src/main/java/com/qa/citizen/rest/EposTransactionsController.java
@@ -1,29 +1,14 @@
 package com.qa.citizen.rest;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.qa.citizen.domain.EposTransactions;
-import com.qa.citizen.service.EposTransactionsService;
 
 @RestController
 @CrossOrigin
 public class EposTransactionsController {
 
-	private EposTransactionsService service;
-
-	public EposTransactionsController(EposTransactionsService service) {
+	public EposTransactionsController() {
 		super();
-		this.service = service;
-	}
-	
-	@GetMapping("/getEposTransactions/{bankCardNumber}")
-	public List<EposTransactions> getAtmTransactionsByCard(@PathVariable Long bankCardNumber) {
-		return this.service.getEposTransactionsByCard(bankCardNumber);
 	}
 
 }
