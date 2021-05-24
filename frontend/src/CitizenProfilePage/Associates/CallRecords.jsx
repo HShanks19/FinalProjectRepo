ad754c8d245e871209aafdb4b7dd35ab91c0a849
holly-shanks <HShanks@qa.com>
2021-05-24 22:47:45 +0100

    Working CallRecords Modal

--- frontend/src/CitizenProfilePage/Associates/CallRecords.jsx
+++ frontend/src/CitizenProfilePage/Associates/CallRecords.jsx
@@ -1,51 +1,75 @@
-const CallRecords = ({ data }) => (
-  <>
-    <tr>
-      <th scope="row">{data.timestamp}</th>
-      <td>{data.receiverName}</td>
-      <td>{data.receiverMSISDN}</td>
-      <button type="button">View Profile</button>
-    </tr>
+import axios from 'axios';
+import { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-    <div>
-      <div className="modal" tabIndex="-1">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Receiver Biography</h5>
-            </div>
-            <div className="modal-body">
-              <h4>
-                Citizen Name:
-                {' '}
-                {data.forenames}
-                {' '}
-                {data.surname}
-              </h4>
-              <h5>
-                Date of Birth:
-                {' '}
-                {data.dateOfBirth}
-              </h5>
-              <h5>
-                Place of Birth:
-                {' '}
-                {data.placeOfBirth}
-              </h5>
-              <h5>
-                Address:
-                {' '}
-                {data.address}
-              </h5>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn-close" data-bs-dismiss="modal" variant="outline-danger" size="lg" aria-label="Close">Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </>
-);
+const CallRecords = ({ data }) => {
+  const [show, setShow] = useState(0);
+  const [forenameM, setForenamesM] = useState('');
+  const [surnameM, setSurnameM] = useState('');
+  const [dOB, setDateOfBirthM] = useState('');
+  const [addressM, setAddressM] = useState('');
+  const [townM, setTownM] = useState('');
+  const [postcodeM, setPostcodeM] = useState('');
+  const closeModal = () => setShow(0);
+  const makeRequest = () => {
+    axios.get(`http://54.74.11.52:5001/getCitizenByNumber/${data.receiverMSISDN}`)
+      .then((response) => {
+        const ModalData = response.data;
+        setShow(1);
+        setForenamesM(ModalData.forenames);
+        setSurnameM(ModalData.surname);
+        setDateOfBirthM(ModalData.dateOfBirth);
+        setAddressM(ModalData.address);
+        setTownM(ModalData.town);
+        setPostcodeM(ModalData.postcode);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleChange = () => {
+    makeRequest();
+  };
+  return (
+    <>
+      <tr>
+        <th scope="row">{data.timestamp}</th>
+        <td>{data.receiverName}</td>
+        <td>{data.receiverMSISDN}</td>
+        <button type="button" onClick={handleChange}>View Profile</button>
+      </tr>
+      <>
+        <Modal show={show}>
+          <Modal.Header>
+            <Modal.Title>Receiver Information</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>
+              Citizen Name:
+              {' '}
+              {forenameM}
+              {' '}
+              {surnameM}
+            </h4>
+            <h5>
+              Date of Birth:
+              {' '}
+              {dOB}
+            </h5>
+            <h5>
+              Address:
+              {' '}
+              {addressM}
+              {townM}
+              {postcodeM}
+            </h5>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>Close Modal</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </>
+  );
+};
 
 export default CallRecords;
--- frontend/src/CitizenProfilePage/Associates/Colleagues.jsx
+++ frontend/src/CitizenProfilePage/Associates/Colleagues.jsx
@@ -1,11 +1,13 @@
 const Colleagues = ({ data }) => (
-  <tbody>
-    <tr>
-      <th scope="row">{data.personName}</th>
-      <td>{data.dateOfBirth}</td>
-      <button type="button">View Profile</button>
-    </tr>
-  </tbody>
+  <>
+    <tbody>
+      <tr>
+        <th scope="row">{data.personName}</th>
+        <td>{data.dateOfBirth}</td>
+        <button type="button">View Profile</button>
+      </tr>
+    </tbody>
+  </>
 );
 
 export default Colleagues;
