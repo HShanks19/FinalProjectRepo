8a1c8ba9d6543fd4e98b45dde8bfff460dfc36b7
holly-shanks <HShanks@qa.com>
2021-05-24 11:40:02 +0100

    Render Info

--- frontend/src/CitizenProfilePage/Biography/Biography.jsx
+++ frontend/src/CitizenProfilePage/Biography/Biography.jsx
@@ -6,13 +6,13 @@ import BiographyRender from './BiographyRender';
 const Biography = () => {
   const [data, setData] = useState([]);
   const makeRequest = () => {
-    axios.get('http://54.74.11.52:5001/getBiographicalInfo/3913177474')
+    axios.get('http://54.74.11.52:5001/getBiographicalInfo/4989925828')
       .then((response) => {
         setData(response.data);
         console.log(data);
       }).catch((err) => console.log(err));
   };
-  useEffect(() => { makeRequest(); }, [data]);
+  useEffect(() => { makeRequest(); }, []);
   const RenderInformation = data.map((d) => <BiographyRender data={d} />);
   return (
     <>
