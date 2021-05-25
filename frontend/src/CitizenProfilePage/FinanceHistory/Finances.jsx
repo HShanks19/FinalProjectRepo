e6a0faa983561d06e3c10e8e8425c86886bb0ac1
holly-shanks <HShanks@qa.com>
2021-05-25 12:18:48 +0100

    Working Error and Loading Financials

--- frontend/src/CitizenProfilePage/FinanceHistory/Finances.jsx
+++ frontend/src/CitizenProfilePage/FinanceHistory/Finances.jsx
@@ -4,26 +4,33 @@ import { useParams, Link } from 'react-router-dom';
 import {
   Button, Row, Col,
 } from 'react-bootstrap';
+import { ThreeDots } from '@agney/react-loading';
 import FinancesRender from './FinancesRender';
+import ErrorMessage from './Error';
 
 const Finances = () => {
   const [financialHistory, setFinancialHistory] = useState([]);
   const { citizenId } = useParams();
+  const [errorVisible, setErrorVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log('citizenId ', citizenId);
   const makeRequest = () => {
+    setLoading(true);
     axios.get(`http://54.74.11.52:5001/getBiographicalInfo/${citizenId}`)
       .then((response) => {
         console.log(response.data);
         axios.post('http://54.74.11.52:5001/getMatchingBankAccounts/', { forenames: `${response.data[0].forenames}`, surname: `${response.data[0].surname}`, dateOfBirth: `${response.data[0].dateOfBirth}` })
           .then((res) => {
             setFinancialHistory(res.data);
-            console.log(financialHistory);
+            setLoading(false);
+            if (res.data.length === 0) {
+              setErrorVisible(true);
+            }
           })
           .catch((err) => console.log(err));
       }).catch((err) => console.log(err));
   };
   useEffect(() => { makeRequest(); }, []);
-
   const RenderFinancesInformation = financialHistory.map((d) => <FinancesRender data={d} />);
   return (
     <>
@@ -73,6 +80,14 @@ const Finances = () => {
           </div>
         </Col>
       </Row>
+      { loading === true
+        && (
+          <ThreeDots className="loading-icon" />
+        )}
+      { errorVisible === true
+        && (
+          <ErrorMessage />
+        )}
       {RenderFinancesInformation}
     </>
   );
